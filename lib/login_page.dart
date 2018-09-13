ea5baeb2301f9960de3fe37865db3588b660e0de
John DeJesus <j.dejesus22@gmail.com>
2018-09-12 21:48:30 -0400

    Design Edits and start new files for login/signup

---
+++ lib/login_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  static String tag = "login-page";
+  State<StatefulWidget> createState() {
+    return _LoginPage();
+  }
+}
+
+class _LoginPage extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,41 @@
 import 'package:flutter/material.dart';
+import 'login_page.dart';
+import 'sign_up.dart';
 
 void main() => runApp(SeidoAudioBook());
 
 //Base of the app starting with the MaterialApp widget
-class SeidoAudioBook extends StatelessWidget {
+class SeidoAudioBook extends StatefulWidget {
   @override
+  State<StatefulWidget> createState() {
+    return _SeidoAudioBook();
+  }
+}
+
+class _SeidoAudioBook extends State<SeidoAudioBook>{
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: const Color.fromRGBO(71,88,123,1.0),
-          title: Text('Seido AudioBooks',
+          title: Center(child: Text('Seido AudioBooks',
               style: new TextStyle(fontWeight: FontWeight.bold),
               textAlign: TextAlign.left),
-        ),
+          )),
         body: Center(
             child: Container(
+              margin: EdgeInsets.all(10.0),
           child: Column(
             children: <Widget>[
               Image.asset('assets/images/seidologo.jpg',),
 
-              Text ('Human Face of Karate',
+              Text ('World Seido Karate Audio App',
                   style: new TextStyle(fontWeight: FontWeight.bold)),
 
-              RaisedButton(onPressed: () {},
+              RaisedButton(onPressed: () {
+                Navigator.of(context).pushNamed(LoginPage.tag);
+              },
               child: Text('Login'), textColor: const Color(0xFFFFFFFF),
               color: const Color.fromRGBO(175,151,105,1.0)),
 
