2803a42e4bfd5b7dd317fc2feb92bec23f856359
John DeJesus <j.dejesus22@gmail.com>
2018-09-13 21:49:19 -0400

    Built up sign_up, login_page, and home_page

--- lib/sign_up.dart
+++ lib/sign_up.dart
@@ -1,1 +1,42 @@
 import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => _SignUpPage();
+}
+
+class _SignUpPage extends State<SignUpPage> {
+
+  String _memberID;
+  String _email;
+  String _password;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      body: Container(
+          padding: EdgeInsets.all(10.0),
+        child: new Form(
+            child: Column(
+              children: <Widget>[
+                new TextFormField(
+                  decoration: new InputDecoration(labelText: 'Member ID')
+                ),
+                new TextFormField(
+                  decoration: new InputDecoration(labelText: 'Email')
+                ),
+                new TextFormField(
+                  decoration: new InputDecoration(labelText: 'Password'),
+                  obscureText: true
+                ),
+              RaisedButton(onPressed: () {},
+                  child: Text('Create Account'),textColor: const Color(0xFFFFFFFF),
+                  color: const Color.fromRGBO(175,151,105,1.0),)
+              ],
+            ))
+      )
+    );
+  }
+}
