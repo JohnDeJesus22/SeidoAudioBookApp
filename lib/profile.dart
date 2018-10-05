d9f3a0f8cd44bfa437794faeb5f09d9b1300ad28
John DeJesus <j.dejesus22@gmail.com>
2018-10-04 22:25:28 -0400

    Start building profile class

---
+++ lib/profile.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Profile{
+
+  String email;
+  String name;
+  String memberid;
+  String bpcheck;
+
+  Profile({
+    this.email,
+    this.name,
+    this.memberid,
+    this.bpcheck
+});
+}
